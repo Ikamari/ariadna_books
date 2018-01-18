a8a12606c691d2c1a297b4bf46b85896d8a4832b
Ikamari <SilverFire098@gmail.com>
2018-01-18 23:26:30 +0200

    Book table styles, hide/show elements button for table

--- app/components/BookList.js
+++ app/components/BookList.js
@@ -7,6 +7,14 @@ import { bindActionCreators } from 'redux';
 const BOOK_TYPES = ['book', 'rick_book', 'note', 'pad']
 
 class BookTable extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            hiddenTableElements: false
+        }
+    }
+
     drawBookElement(bookData, index) {
         return (
             <div className='book-table-element' key={`book-${index}`}>
@@ -23,19 +31,33 @@ class BookTable extends Component {
         )
     }
 
+    hideTableElements() {
+        this.setState({
+            hiddenTableElements: !this.state.hiddenTableElements
+        })
+    }
+
     render() {
         const { books } = this.props;
-        console.log("***", books);
+        const hidden = this.state.hiddenTableElements;
         return (
-            <div className='book-table'>
-                <div className='book-table-element'>
-                    <span className='book-index'>ID</span>
-                    <span className='book-title'>Название</span>
-                    <span className='book-type'>Тип</span>
-                    <div className='button-wrapper'/>
-                    <div className='button-wrapper'/>
-                </div>
-                {books.map((bookData, index) => this.drawBookElement(bookData, index))}
+            <div className={'book-table' + (hidden ? ' hidden-elements' : '')}>
+                {!hidden ?
+                    <div className='book-table-element'>
+                        <span className='book-index'>ID</span>
+                        <span className='book-title'>Название</span>
+                        <span className='book-type'>Тип</span>
+                        <div className='button-wrapper'/>
+                        <div className='button-wrapper'>
+                            <div className='hide-button' onClick={() => this.hideTableElements()}>Скрыть</div>
+                        </div>
+                    </div> :
+                    <div className='button-wrapper'>
+                        <div className='hide-button' onClick={() => this.hideTableElements()}>Показать</div>
+                    </div>
+                }
+                {!hidden ?
+                    books.map((bookData, index) => this.drawBookElement(bookData, index)) : null}
             </div>
         )
     }
